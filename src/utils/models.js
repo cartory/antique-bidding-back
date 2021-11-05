56e2ab6ed46c3989a349cb2a372b5233b8521db6
cartory <xXcartoryXx@gmail.com>
2021-11-05 07:51:35 -0400

    update models > add rels

--- src/utils/models.js
+++ src/utils/models.js
@@ -1,1 +1,22 @@
-const Bot = require('../models/Bot')const User = require('../models/User')const Role = require('../models/Role')const Antique = require('../models/Antique')const User_Antique = require('../models/User_Antique')module.exports = {	Bot,	User,	Role,	Antique,	User_Antique,}
+const Bot = require('../models/Bot')
+const User = require('../models/User')
+const Role = require('../models/Role')
+const Antique = require('../models/Antique')
+const User_Antique = require('../models/User_Antique')
+
+Role.hasMany(User, { foreignKey: 'Roleid', as: 'users' })
+User.belongsTo(Role, { foreignKey: 'Roleid', as: 'role' })
+
+// User.hasOne(Bot, { foreignKey: 'Botid', as: 'bot' })
+// Bot.belongsTo(User, { foreignKey: 'Userid', as: 'user' })
+
+User.belongsToMany(Antique, { foreignKey: 'Userid', as: 'users' })
+Antique.belongsToMany(User, { foreignKey: 'Antiqueid', as: 'antiques' })
+
+module.exports = {
+	Bot,
+	User,
+	Role,
+	Antique,
+	User_Antique,
+}
