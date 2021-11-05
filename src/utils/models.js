4ce0e81ced562ebe1d8241e62a5b77dd106c9462
cartory <xXcartoryXx@gmail.com>
2021-11-05 07:56:01 -0400

    update models > Category

--- src/utils/models.js
+++ src/utils/models.js
@@ -2,13 +2,17 @@ const Bot = require('../models/Bot')
 const User = require('../models/User')
 const Role = require('../models/Role')
 const Antique = require('../models/Antique')
+const Category = require('../models/Category')
 const User_Antique = require('../models/User_Antique')
 
 Role.hasMany(User, { foreignKey: 'Roleid', as: 'users' })
 User.belongsTo(Role, { foreignKey: 'Roleid', as: 'role' })
 
-// User.hasOne(Bot, { foreignKey: 'Botid', as: 'bot' })
-// Bot.belongsTo(User, { foreignKey: 'Userid', as: 'user' })
+User.hasOne(Bot, { foreignKey: 'Userid', as: 'bot' })
+Bot.belongsTo(User, { foreignKey: 'Userid', as: 'user' })
+
+Category.hasMany(Antique, { foreignKey: 'Categoryid', as: 'antiques' })
+Antique.belongsTo(Category, { foreignKey: 'Categoryid', as: 'category' })
 
 User.belongsToMany(Antique, { foreignKey: 'Userid', as: 'users' })
 Antique.belongsToMany(User, { foreignKey: 'Antiqueid', as: 'antiques' })
@@ -18,5 +22,6 @@ module.exports = {
 	User,
 	Role,
 	Antique,
+	Category,
 	User_Antique,
 }
