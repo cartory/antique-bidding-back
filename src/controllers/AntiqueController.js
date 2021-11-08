7f5861b466e7ca2f122fe1144b49957772fa68ae
cartory <xXcartoryXx@gmail.com>
2021-11-08 03:19:06 -0400

    update antiqueController > makeBid, get

--- src/controllers/AntiqueController.js
+++ src/controllers/AntiqueController.js
@@ -1,11 +1,56 @@
 const { Op } = require('sequelize')
 
 const { Controller } = require('../utils/controller')
-const { Antique } = require('../utils/models')
+const { Antique, User_Antique } = require('../utils/models')
 
 class AntiqueController extends Controller {
 	constructor() {
 		super(Antique)
+	}
+
+	get = async (Antiqueid) => {
+		try {
+			const antiqueResult = await User_Antique.findOne({
+				where: { Antiqueid },
+				attributes: {
+					exclude: ['Categoryid']
+				},
+				include: [
+					'category',
+					'antiques',
+				],
+			})
+
+			return antiqueResult.toJSON()
+		} catch (err) {
+			console.error(err);
+		}
+
+		return null
+	}
+
+	makeBid = async ({ Userid, Antiqueid, price }) => {
+		try {
+			let lastBidResponse
+
+			const userAntique = await User_Antique.findOne({
+				where: { Userid, Antiqueid }
+			})
+
+			if (userAntique) {
+				lastBidResponse = await userAntique.update({ lastBid: price })
+			} else {
+				lastBidResponse = await User_Antique.create({
+					Userid, Antiqueid, lastBid: price
+				})
+			}
+
+			console.log(lastBidResponse.toJSON())
+			return this.get(Antiqueid)
+		} catch (err) {
+			console.error(err)
+		}
+
 	}
 
 	all = async ({ query }, res) => {
