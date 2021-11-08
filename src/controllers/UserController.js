37334573849264917ad31f24170353c14722e182
cartory <xXcartoryXx@gmail.com>
2021-11-08 00:15:06 -0400

    update userController > save user with relations

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,38 @@
 const { Controller } = require('../utils/controller')
-const { User } = require('../utils/models')
+const { User, Bot, Role } = require('../utils/models')
 
+let Roleid
 class UserController extends Controller {
-	constructor() {		super(User)	}}
+	constructor() {
+		super(User)
+	}
+
+	save = async ({ body }, res) => {
+		const { name, photoUrl, email } = body
+
+		try {
+			let user = await User.findOne({
+				where: { email },
+				include: ['bot', 'role']
+			})
+
+			if (!Roleid) {
+				const role = await Role.findOne({ where: { name: 'user' } })
+				Roleid = role.getDataValue('id')
+			}
+
+			if (!user) {
+				user = await User.create({ name, photoUrl, email, Roleid })
+				await Bot.create({ Userid: user.getDataValue('id') })
+			}
+
+			return res.status(200).json(user)
+		} catch (err) {
+			console.error(err);
+		}
+
+		return res.status(500).json(this.defaultErrorMessage)
+	}
+}
 
 module.exports = new UserController()
