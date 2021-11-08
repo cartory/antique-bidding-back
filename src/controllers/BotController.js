5a155b4de5f88a4534e24344b1433c845f4d918e
cartory <xXcartoryXx@gmail.com>
2021-11-08 08:46:24 -0400

    update botController > save

--- src/controllers/BotController.js
+++ src/controllers/BotController.js
@@ -2,6 +2,20 @@ const { Controller } = require('../utils/controller')
 const { Bot } = require('../utils/models')
 
 class BotController extends Controller {
-	constructor() {		super(Bot)	}}
+	constructor() {
+		super(Bot)
+	}
+
+	save = async ({ body }, res) => {
+		try {
+			const [bot] = await Bot.upsert(body)
+			return res.status(200).json(bot)
+		} catch (err) {
+			console.error(err)
+		}
+
+		return res.status(500).json(this.defaultErrorMessage)
+	}
+}
 
 module.exports = new BotController()
